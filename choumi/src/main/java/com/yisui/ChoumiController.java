3731f54fe7445a42d2f71a3a1d58703542a5f643
constling <1005993786@qq.com>
2019-09-29 07:30:40 +0800

    kd

---
+++ choumi/src/main/java/com/yisui/ChoumiController.java
@@ -0,0 +1,9 @@
+package com.yisui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hi")
+public class ChoumiController {
+}
