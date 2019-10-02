d686704361574a2d88f506e40525545901a09a81
lingxing.lx <lingxing.lx@alibaba-inc.com>
2019-10-02 16:23:06 +0800

    homepage

--- choumi/src/main/java/com/yisui/ChoumiController.java
+++ choumi/src/main/java/com/yisui/ChoumiController.java
@@ -8,6 +8,13 @@ public class ChoumiController {
 
     @RequestMapping("/toHI")
     public String SayHi() {
+        System.out.println("say hiiii");
         return "sayHi";
+    }
+
+    @RequestMapping("/index")
+    public String HomePage() {
+        System.out.println("1111111111");
+        return "index";
     }
 }
